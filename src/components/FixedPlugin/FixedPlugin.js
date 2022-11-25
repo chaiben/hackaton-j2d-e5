bdfde3226312726c0578cb33f1091b2cb83bee6e
Marçal Machado Chaiben <marcal.chaiben@qustodio.com>
2022-11-25 08:02:59 +0100

    Small tests

--- src/components/FixedPlugin/FixedPlugin.js
+++ src/components/FixedPlugin/FixedPlugin.js
@@ -22,7 +22,7 @@ import { Dropdown, Badge, Button, Form } from "react-bootstrap";
 
 import sideBarImage1 from "assets/img/sidebar-1.jpg";
 import sideBarImage2 from "assets/img/sidebar-2.jpg";
-import sideBarImage3 from "assets/img/sidebar-3.jpg";
+import sideBarImage3 from "assets/img/logistics.jpg";
 import sideBarImage4 from "assets/img/sidebar-4.jpg";
 
 function FixedPlugin({
